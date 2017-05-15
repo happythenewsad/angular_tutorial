400a8b32fada271abd1cfc2884e7f58c61a79b16
happythenewsad <peter@peterkong.com>
2017-05-15 14:44:48 -0400

    git ignoring js and js.map files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,7 +76,7 @@ export class AppComponent implements OnInit {
 	title = 'A Tour of Heroes';
 
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   selectedHero: Hero;
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,4 +9,15 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+  	return new Promise(resolve => {
+  	  setTimeout(() => resolve(this.getHeroes()), 500);
+    });
+  }
 }
+
+
+
+
+
