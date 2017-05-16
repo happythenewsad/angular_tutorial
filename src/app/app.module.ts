61810b449485c5afa09f6498575a8ea4d233cb3c
Peter Kong <peter@peterkong.com>
2017-05-16 13:31:23 -0400

    halfway through part 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,56 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component'
+
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard.component'
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,
-  				  HeroDetailComponent ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ]),
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ]),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
+
+  ],
+  declarations: [ 
+    AppComponent,
+    DashboardComponent,
+  	HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ 
+    HeroService
+  ],
+
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
